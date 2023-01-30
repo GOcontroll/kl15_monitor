a903882235020d77e2c60aa5118c7c7ba9858edf
SpieringsAE <ben_spierings@hotmail.com>
2023-01-30 14:24:18 +0100

    adding modularity for different controllers
    now takes an argument to determine which channels of the mcp3004 to check.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 external kl15 monitor to automatically shut down the controller
 
 To build on Linux, download the aarch64-none-linux-gun-gcc compiler and set it up, enter the directory and run make.
+
+arguments: 0 = moduline iv, 1 = moduline mini, 2 = moduline display.
--- main.c
+++ main.c
@@ -1,14 +1,17 @@
 #include "iio.h"
 #include <unistd.h>
 #include <stdint.h>
-#include <stdio.h>
+
+#define MODULINE4 0
+#define MODULINEMINI 1
+#define MODULINEDISP 2
 
 struct iio_context *iioContext;
 struct iio_device *iioMCP;
 const struct iio_channel *adcChannels[4];
 
 uint16_t GocontrollProcessorboardSupply_ReadAdc(uint8_t supply) {
-		char buf[100];
+		char buf[5];
 		size_t bufSize = sizeof buf;
 		switch (supply) {
 			//read raw (voltage) attributes
@@ -22,21 +25,30 @@ uint16_t GocontrollProcessorboardSupply_ReadAdc(uint8_t supply) {
 			iio_channel_attr_read(adcChannels[1], "raw", buf, bufSize);
 			break;
 		}
-		//convert to milivolts and store it
+		//convert to milivolts and return it
 		return (uint16_t)((float)(strtof(buf, NULL) * 25.54)); //25.54 = ((3.35/1023)/1.5)*11700	
 }
 
-int main(void){
-
-
+int main(int argc, char** argv){
     uint16_t kl15 = 10000;
     uint8_t count = 0;
-    
+    int type;
+
+    //get the controller type from the argument or default to moduline 4
+    if (argc == 2) {
+        type = atoi(argv[1]);
+    } else {
+        type = 0;
+    }
     iioContext = iio_create_local_context();
-    /* set up the mcp3004 iio channels*/
-	
+
+    // get the mcp3004 from the context
     uint8_t channel_count;
     iioMCP = iio_context_find_device(iioContext, "mcp3004");
+    if (!iioMCP) {
+        exit(EXIT_FAILURE);
+    }
+
     //loop through the channels available to this device
     for (uint8_t i = 0; i < iio_device_get_channels_count(iioMCP); ++i) {
         struct iio_channel *chn = iio_device_get_channel(iioMCP, i);
@@ -46,18 +58,32 @@ int main(void){
             channel_count++;
         }
     }
+
+    //set the number of kl15s present on this controller
+    uint8_t kl15s;
+    switch (type) {
+        case MODULINE4 ... MODULINEMINI:
+            kl15s=3;
+            break;
+        case MODULINEDISP:
+            kl15s=1;
+            break;
+        default:
+            kl15s=3;
+    }
 	
+    //endless loop till the controller shuts down or the program gets terminated from the outside
     while (count <= 10) {
         if (system("systemctl is-active go-simulink")) {
-            for(uint8_t i = 0; i <3; i++) {
+            for(uint8_t i = 0; i <kl15s; i++) {
                 if (GocontrollProcessorboardSupply_ReadAdc(i) > 5000) {
                     count = 0;
                     break;
-                } else if (i==2){
+                } else if (i==kl15s-1){
                     count ++;
                 }
             }
-        }
+        } else {sleep(5);} //if simulink is active lower the checking rate as it is assumed simulink will shut down the controller when necessary
         usleep(500000);
     }
     system("shutdown now");
