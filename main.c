87c87dd33dcfd0248571778309cda2836bb3e4c3
SpieringsAE <ben_spierings@hotmail.com>
2023-04-06 09:51:06 +0200

    buffer for adc readout was to small causing shutdown at high voltage

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include "iio.h"
 #include <unistd.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #define MODULINE4 0
 #define MODULINEMINI 1
@@ -11,29 +13,27 @@ struct iio_device *iioMCP;
 const struct iio_channel *adcChannels[4];
 
 uint16_t GocontrollProcessorboardSupply_ReadAdc(uint8_t supply) {
-		char buf[5];
-		size_t bufSize = sizeof buf;
-		switch (supply) {
-			//read raw (voltage) attributes
-			case 0:
-			iio_channel_attr_read(adcChannels[3], "raw", buf, bufSize);
-			break;
-			case 1:
-			iio_channel_attr_read(adcChannels[0], "raw", buf, bufSize);
-			break;
-			case 2:
-			iio_channel_attr_read(adcChannels[1], "raw", buf, bufSize);
-			break;
-		}
-		//convert to milivolts and return it
-		return (uint16_t)((float)(strtof(buf, NULL) * 25.54)); //25.54 = ((3.35/1023)/1.5)*11700	
+    char buf[6];
+    size_t bufSize = sizeof buf;
+    switch (supply) {
+        //read raw (voltage) attributes
+        case 0:
+        iio_channel_attr_read(adcChannels[3], "raw", buf, bufSize);
+        break;
+        case 1:
+        iio_channel_attr_read(adcChannels[0], "raw", buf, bufSize);
+        break;
+        case 2:
+        iio_channel_attr_read(adcChannels[1], "raw", buf, bufSize);
+        break;
+    }
+
+    return (uint16_t)((float)(strtof(buf, NULL) * 25.54)); //25.54 = ((3.35/1023)/1.5)*11700
 }
 
 int main(int argc, char** argv){
-    uint16_t kl15 = 10000;
     uint8_t count = 0;
     int type;
-
     //get the controller type from the argument or default to moduline 4
     if (argc == 2) {
         type = atoi(argv[1]);
@@ -43,11 +43,12 @@ int main(int argc, char** argv){
     iioContext = iio_create_local_context();
 
     // get the mcp3004 from the context
-    uint8_t channel_count;
     iioMCP = iio_context_find_device(iioContext, "mcp3004");
     if (!iioMCP) {
+        fprintf(stderr,"Unable to get IIO device.");
         exit(EXIT_FAILURE);
     }
+    uint8_t channel_count = 0;
 
     //loop through the channels available to this device
     for (uint8_t i = 0; i < iio_device_get_channels_count(iioMCP); ++i) {
@@ -58,6 +59,10 @@ int main(int argc, char** argv){
             channel_count++;
         }
     }
+
+    if (channel_count < 4) {
+        fprintf(stderr,"Invalid ADC channel count.");
+        exit(EXIT_FAILURE);}
 
     //set the number of kl15s present on this controller
     uint8_t kl15s;
@@ -71,9 +76,9 @@ int main(int argc, char** argv){
         default:
             kl15s=3;
     }
-	
+
     //endless loop till the controller shuts down or the program gets terminated from the outside
-    while (count <= 10) {
+    while (count <= 20) {
         if (system("systemctl is-active go-simulink")) {
             for(uint8_t i = 0; i <kl15s; i++) {
                 if (GocontrollProcessorboardSupply_ReadAdc(i) > 5000) {
